0b7f63afb2615e17b4f1c1c00a64fabc2d216ba1
akramul47 <akramul47@gmail.com>
2022-03-03 14:41:27 +0600

    Implemented map for table data

--- lib/widgets.dart/scrollable-column-widget.dart
+++ lib/widgets.dart/scrollable-column-widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/data.dart';
 
 class ScrollableColumnWidget extends StatelessWidget {
   @override
@@ -7,269 +8,73 @@ class ScrollableColumnWidget extends StatelessWidget {
       child: SingleChildScrollView(
         scrollDirection: Axis.horizontal,
         child: DataTable(
-          headingRowColor: MaterialStateProperty.all(Colors.green[100]),
-          columnSpacing: 40,
-          decoration: BoxDecoration(
-            border: Border(
-              right: BorderSide(
-                color: Colors.grey,
-                width: 0.5,
+            headingRowColor: MaterialStateProperty.all(Colors.green[100]),
+            columnSpacing: 40,
+            decoration: BoxDecoration(
+              border: Border(
+                right: BorderSide(
+                  color: Colors.grey,
+                  width: 0.5,
+                ),
               ),
             ),
-          ),
-          columns: const <DataColumn>[
-            DataColumn(
-                label: Text(
-              'Stock',
-            )),
-            DataColumn(
-                label: Text(
-              'Average Cost',
-            )),
-            DataColumn(
-                label: Text(
-              'Market Rate',
-            )),
-            DataColumn(
-                label: Text(
-              'Profit',
-            )),
-            DataColumn(
-                label: Text(
-              'Buy Cost',
-            )),
-            DataColumn(
-                label: Text(
-              'Market Value',
-            )),
-            DataColumn(
-                label: Text(
-              'Code',
-            )),
-          ],
-          rows: const <DataRow>[
-            DataRow(
-              cells: <DataCell>[
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-              ],
-            ),
-            DataRow(
-              cells: <DataCell>[
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-              ],
-            ),
-            DataRow(
-              cells: <DataCell>[
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-              ],
-            ),
-            DataRow(
-              cells: <DataCell>[
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-              ],
-            ),
-            DataRow(
-              cells: <DataCell>[
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-              ],
-            ),
-            DataRow(
-              cells: <DataCell>[
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-              ],
-            ),
-            DataRow(
-              cells: <DataCell>[
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-              ],
-            ),
-            DataRow(
-              cells: <DataCell>[
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-              ],
-            ),
-            DataRow(
-              cells: <DataCell>[
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-              ],
-            ),
-            DataRow(
-              cells: <DataCell>[
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-              ],
-            ),
-            DataRow(
-              cells: <DataCell>[
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-              ],
-            ),
-            DataRow(
-              cells: <DataCell>[
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-              ],
-            ),
-            DataRow(
-              cells: <DataCell>[
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-              ],
-            ),
-            DataRow(
-              cells: <DataCell>[
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-              ],
-            ),
-            DataRow(
-              cells: <DataCell>[
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-              ],
-            ),
-            DataRow(
-              cells: <DataCell>[
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-              ],
-            ),
-            DataRow(
-              cells: <DataCell>[
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-              ],
-            ),
-            DataRow(
-              cells: <DataCell>[
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-              ],
-            ),
-            DataRow(
-              cells: <DataCell>[
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-              ],
-            ),
-            DataRow(
-              cells: <DataCell>[
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-                DataCell(Text('AGNISYS')),
-              ],
-            ),
-          ],
-        ),
+            columns: const <DataColumn>[
+              DataColumn(
+                  label: Text(
+                'Stock',
+              )),
+              DataColumn(
+                  label: Text(
+                'Average Cost',
+              )),
+              DataColumn(
+                  label: Text(
+                'Market Rate',
+              )),
+              DataColumn(
+                  label: Text(
+                'Profit',
+              )),
+              DataColumn(
+                  label: Text(
+                'Buy Cost',
+              )),
+              DataColumn(
+                  label: Text(
+                'Market Value',
+              )),
+              DataColumn(
+                  label: Text(
+                'Code',
+              )),
+            ],
+            rows: [
+              ...symbolData.map((symbol) => DataRow(
+                    cells: [
+                      DataCell(Container(
+                          alignment: AlignmentDirectional.center,
+                          child: Text(symbol.totalStock.toString()))),
+                      DataCell(Container(
+                          alignment: AlignmentDirectional.center,
+                          child: Text(symbol.averageCost.toString()))),
+                      DataCell(Container(
+                          alignment: AlignmentDirectional.center,
+                          child: Text(symbol.marketRate.toString()))),
+                      DataCell(Container(
+                          alignment: AlignmentDirectional.center,
+                          child: Text(symbol.profit.toString()))),
+                      DataCell(Container(
+                          alignment: AlignmentDirectional.center,
+                          child: Text(symbol.buyCost.toString()))),
+                      DataCell(Container(
+                          alignment: AlignmentDirectional.center,
+                          child: Text(symbol.marketValue.toString()))),
+                      DataCell(Container(
+                          alignment: AlignmentDirectional.center,
+                          child: Text(symbol.code.toString()))),
+                    ],
+                  ))
+            ]),
       ),
     );
   }
